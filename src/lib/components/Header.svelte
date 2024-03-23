8054944e73a6c1a0642710475114696d495dd487
vishaalram02 <vishaalram02@gmail.com>
2024-03-23 10:24:48 -0700

    hello evan

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,34 @@
+<script>
+	const links = [
+		{ name: 'Rush', url: '/rush' },
+		{ name: 'Events', url: '/events' },
+		{ name: 'Brothers', url: '/brothers' },
+		{ name: 'House', url: '/house' },
+		{ name: 'History', url: '/history' },
+		{ name: 'Alumni', url: '/alumni' }
+	];
+	let y = 0;
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<header
+	class="px-40 fixed w-screen z-20"
+	style="background-color: rgba(75,158,252,{Math.min(y / 200, 1)});"
+>
+	<div class="flex justify-between text-white py-3">
+		<a href="/" class="text-4xl font-extrabold text-center flex font-book">
+			<img class="h-10 mr-3" src="./images/zbt-crest.png" alt="zbt-crest" />
+			<p>ZBT</p>
+		</a>
+		<div class="flex justify-around space-x-1">
+			{#each links as link}
+				<a
+					href={link.url}
+					class="font-nunito text-2xl font-bold flex items-center px-3 transition-colors rounded-xl hover:bg-cyan-600"
+					>{link.name}</a
+				>
+			{/each}
+		</div>
+	</div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let title = 'ZETA BETA TAU';
+	let display = 'Z';
+
+	const type = (text: string, i: number) => {
+		const interval = setTimeout(() => {
+			display = text.slice(0, i);
+			if (i < text.length) {
+				type(text, i + 1);
+				clearInterval(interval);
+			}
+		}, 100);
+	};
+
+	onMount(() => {
+		type(title, 1);
+	});
+	let y = 0;
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<div
+	class="absolute -z-10 bg-[url('/images/house.jpeg')] bg-no-repeat bg-cover bg-fixed h-screen w-screen"
+/>
+<div class="relative -z-10 h-screen w-screen text-white drop-shadow-[0px_10px_5px_rgba(0,0,0,1)]">
+	<div class="h-full flex flex-col items-center justify-center" style="padding-top: {y}px">
+		<p class="text-5xl font-nunito font-bold">WELCOME TO</p>
+		<p class="text-[120px] font-book font-bold -mt-4">{display}</p>
+		<p class="text-5xl font-nunito font-bold">MIT Xi Chapter</p>
+	</div>
+</div>
+
+<div class="px-40 pt-20 relative h-[800px] bg-stone-300 z-10 text-black">
+	<div class="flex space-x-10">
+		<div>
+			<p class="text-5xl font-book font-black">Lifelong Brotherhood</p>
+			<p class="pt-5 font-nunito text-3xl">
+				ZBT has existed at MIT for over 100 years, and spent the last 50 of those nestled away at
+				our home in Brookline. We are a brotherhood of over 50 members from across the United States
+				and various foreign countries, representing a diverse array of majors at MIT and a broad
+				range of on campus activities.
+			</p>
+		</div>
+		<img class="w-1/2 rounded-xl" src="./images/zbt-stairs.jpeg" alt="zbt-stairs" />
+	</div>
+</div>
